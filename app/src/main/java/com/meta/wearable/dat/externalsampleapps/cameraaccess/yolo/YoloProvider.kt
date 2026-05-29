517171d6437456e359f151b64624e6a915f59c60
TomasConti02 <tomasconti34@gmail.com>
2026-05-29 19:08:02 +0200

    better yolo provider

--- app/src/main/java/com/meta/wearable/dat/externalsampleapps/cameraaccess/yolo/YoloProvider.kt
+++ app/src/main/java/com/meta/wearable/dat/externalsampleapps/cameraaccess/yolo/YoloProvider.kt
@@ -3,26 +3,32 @@ package com.meta.wearable.dat.externalsampleapps.cameraaccess.yolo
 import android.content.Context
 import kotlinx.coroutines.*
 /*
-Try to implement the patter singleton factory, only one instance of the yolo detector into the heap
-of the process. the load operation is blocking and synch, better asynch
+Try to implement the patter singleton factory,
+only one instance of the yolo detector into the gpu.
+The load operation is blocking and synch, better asynch
 */
-object YoloProvider {
-    @Volatile
+object YoloProvider { //Singleton
+    @Volatile     //All the thread ( UI thread) of the process can access to the variabile as soon as available
     private var instance: YoloDetector? = null
-    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO) //Dispatchers.IO thread pool for corutine good for loading operations
-    private var initJob: Deferred<YoloDetector>? = null
+    private val scope = CoroutineScope(Dispatchers.IO) //Dispatchers.IO thread pool for corutine good for loading operations
+    //private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private var initJob: Deferred<YoloDetector>? = null //waiting ticket of the asynch corutine
     fun initAsync(context: Context): Deferred<YoloDetector> {
-        instance?.let {
+
+        instance?.let { //object is already available, return it
             return CompletableDeferred(it)
         }
-        initJob?.let {
-            return it
+
+        initJob?.let { //if the object already exist do not create another
+            return it //get out the waiting reference
         }
+
         initJob = scope.async { //coruntine launch
             val detector = YoloDetector(context.applicationContext)
             instance = detector
             detector
         }
+
         return initJob!!
     }
     //need a asynch loading operation because don't want to block the main/ui thread
